516424bd85e17d483b42456cafaf556838ab25af
Brian <mulindijrn@gmail.com>
2025-01-01 23:21:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex items-center justify-center text-4xl font-bold">
+        Hello World
+      </h1>      
+    </>
+  )
+}
+
+export default App
