e8c147a81b5fb0d48a27e2eadc66e3b2227e5bfe
Brian <mulindijrn@gmail.com>
2025-01-05 23:57:25 +0300

    Add a description about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,9 @@ const About = () => {
         <div className=''>
           <img src={Potrait} alt="Mulindi Jr." className='h-[420px] w-96 rounded-md'/>
         </div>
+        <div className='w-1/2 rounded-md'>
+        <p className='text-xl'>I'm a full-stack developer specializing in the MERN stack (MongoDB, Express, React, Node.js). I build scalable and dynamic web applications, focusing on creating seamless user experiences. I'm always looking to learn and grow while contributing to exciting projects.</p>
+        </div>
       </div>
     </div>
   )
