c4aa167173746eb4634221b4e13f1bca88a63775
Brian <mulindijrn@gmail.com>
2025-01-08 22:40:13 +0300

    feat:Add a footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='border-t mx-44'>
+      <div className='text-center p-4'>
+        <p>&copy; 2025 Mulindi Jr. All Rights Reserved.</p>           
+      </div>
+    </div>
+  )
+}
+
+export default Footer
