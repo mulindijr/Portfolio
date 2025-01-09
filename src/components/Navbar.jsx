3ea12628f785ddc3990cd1782c9e46b2a6ca096e
Brian <mulindijrn@gmail.com>
2025-01-09 14:45:08 +0300

    Feat: Add smooth scrolling to Navbar links using AnchorLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Navbar = () => {
   const [menu, setMenu] = useState('home')
 
   return (
     <div className='flex justify-between items-center mt-5 mx-44'>
-      <h1 className='cursor-pointer text-4xl font-semibold bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent'>Mulindi Jr.</h1>
+      <h1 className='cursor-pointer text-4xl font-semibold bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent'><AnchorLink href='#home'>Mulindi Jr.</AnchorLink></h1>
       <ul className='flex space-x-10 text-[20px]'>
-        <li className={`cursor-pointer ${menu==='home' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('home')}>Home</li>
-        <li className={`cursor-pointer ${menu==='about' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('about')}>About</li>
-        <li className={`cursor-pointer ${menu==='services' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('services')}>Services</li>
-        <li className={`cursor-pointer ${menu==='portfolio' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('portfolio')}>Portfolio</li>
-        <li className={`cursor-pointer ${menu==='contact' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('contact')}>Contact</li>
+        <li className={`cursor-pointer ${menu==='home' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('home')}> <AnchorLink href='#home'>Home</AnchorLink></li>
+        <li className={`cursor-pointer ${menu==='about' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('about')}> <AnchorLink href='#about' offset={50}>ABout</AnchorLink></li>
+        <li className={`cursor-pointer ${menu==='services' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('services')}> <AnchorLink href='#services' offset={50}>Services</AnchorLink></li>
+        <li className={`cursor-pointer ${menu==='portfolio' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('portfolio')}> <AnchorLink href='#porfolio' offset={50}>Porfolio</AnchorLink></li>
+        <li className={`cursor-pointer ${menu==='contact' ? 'text-pink-500' : 'text-white'}`}onClick={() => setMenu('contact')}> <AnchorLink href='#contact' offset={50}>Contact</AnchorLink></li>
       </ul>
-      <button className='py-4 px-8 rounded-[50px] text-[20px] bg-gradient-to-r from-pink-500 to-purple-500'>Connect With Me</button>
+      <button className='py-4 px-8 rounded-[50px] text-[20px] bg-gradient-to-r from-pink-500 to-purple-500'><AnchorLink href='#contact' offset={50}>Connect With Me</AnchorLink></button>
     </div>
   )
 }
