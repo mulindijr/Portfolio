ebbec5886e06c67866db52dd885a026d30ae7305
Brian <mulindijrn@gmail.com>
2025-01-09 14:51:39 +0300

    feat: group Navbar and Hero components under HomeSection with a shared id 'home'

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+
+const HomeSection = () => {
+  return (
+    <div id='home'>
+        <Navbar />
+        <Hero />
+    </div>
+  )
+}
+
+export default HomeSection
