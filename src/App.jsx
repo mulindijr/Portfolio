15fd93a194339efcce04a53ed39b08f970465b23
Brian <mulindijrn@gmail.com>
2025-01-11 14:45:38 +0300

    Import Back to Top button to the main component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from "./components/Services"
 import Porfolio from "./components/Porfolio"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import BackToTopBtn from "./components/BackToTopBtn"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Porfolio />
       <Contact />  
       <Footer />  
+      <BackToTopBtn />
     </>
   )
 }
