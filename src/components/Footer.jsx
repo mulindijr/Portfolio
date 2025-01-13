20b201bf3c2d9618f881c66967423d5f7f71b29b
Brian <mulindijrn@gmail.com>
2025-01-13 17:31:31 +0300

    Make Footer Section mobile responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='border-t mx-44'>
+    <div className='border-t mx-8 sm:mx-44'>
       <div className='text-center p-4'>
         <p>&copy; 2025 Mulindi Jr. All Rights Reserved.</p>           
       </div>
