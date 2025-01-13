b5ab2f37b13f2a5694cdd64671baf7e3785b4a06
Brian <mulindijrn@gmail.com>
2025-01-13 03:57:49 +0300

    close mobile menu when a navigation link is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,27 +41,27 @@ const Navbar = () => {
       <div className="relative">
         <ul className= {`${mobileMenu ? 'flex' : 'hidden' } absolute right-0 top-8 bg-[#161513] pt-2 px-20 h-screen sm:h-auto sm:pt-0 sm:px-0 sm:relative sm:top-0 sm:right-0 sm:bg-transparent sm:flex flex-col sm:flex-row sm:space-x-10 space-y-4 sm:space-y-0 text-[20px]`}>
           <li className={`cursor-pointer ${activeSection === 'home' ? 'text-pink-500' : 'text-white'}`}>
-            <Link to="home" smooth={true} duration={1500} spy={true} onSetActive={() => handleSetActive('home')}>
+            <Link to="home" smooth={true} duration={1500} spy={true} onSetActive={() => handleSetActive('home')} onClick={toggleMenu}>
               Home
             </Link>
           </li>
           <li className={`cursor-pointer ${activeSection === 'about' ? 'text-pink-500' : 'text-white'}`}>
-            <Link to="about" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('about')}>
+            <Link to="about" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('about')} onClick={toggleMenu}>
               About
             </Link>
           </li>
           <li className={`cursor-pointer ${activeSection === 'services' ? 'text-pink-500' : 'text-white'}`}>
-            <Link to="services" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('services')}>
+            <Link to="services" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('services')} onClick={toggleMenu}>
               Services
             </Link>
           </li>
           <li className={`cursor-pointer ${activeSection === 'portfolio' ? 'text-pink-500' : 'text-white'}`}>
-            <Link to="portfolio" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('portfolio')}>
+            <Link to="portfolio" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('portfolio')} onClick={toggleMenu}>
               Portfolio
             </Link>
           </li>
           <li className={`cursor-pointer ${activeSection === 'contact' ? 'text-pink-500' : 'text-white'}`}>
-            <Link to="contact" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('contact')}>
+            <Link to="contact" smooth={true} duration={1500} spy={true} offset={-100} onSetActive={() => handleSetActive('contact')} onClick={toggleMenu}>
               Contact
             </Link>
           </li>
